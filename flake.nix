677069e21232b114d5d42c09429176d073e4ce0b
Ruben <ruben.beck@pm.me>
2026-07-07 13:14:38 +0200

    feat: Create ln-service module

--- flake.nix
+++ flake.nix
@@ -1,9 +1,19 @@
 {
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+  inputs.nix-bitcoin.url = "github:fort-nix/nix-bitcoin/release";
+  inputs.nix-bitcoin.inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { self, nixpkgs, nix-bitcoin }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      nixos-lib = import (nixpkgs + "/nixos/lib") { };
+      mkTest = imports: system: nixos-lib.runTest {
+      inherit imports;
+      hostPkgs = import nixpkgs {inherit system;};
+      # Available both to the test module and to node modules.
+      node.specialArgs = { inherit nix-bitcoin; };
+      _module.args = { inherit nix-bitcoin; };
+    };
     in
     {
       legacyPackages = forAllSystems (system: import ./default.nix {
@@ -14,5 +24,8 @@
       # homeModules = import ./home-modules;
       # darwinModules = import ./darwin-modules;
       # flakeModules = import ./flake-modules;
+      checks = forAllSystems (system: {
+        ln-service = mkTest [./tests/ln-service.nix] system;
+      });
     };
 }
